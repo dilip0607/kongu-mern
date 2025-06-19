9b9fb85b31d7c981f2d1b3ca1ffdf4e0bb251b6b
dilip0607 <dilipbabu9487@gmail.com>
2025-06-19 16:06:28 +0530

    thrid

---
+++ day1/practise.js
@@ -0,0 +1,39 @@
+let a=0;
+let name ="tamil";
+let isbool="ture";
+let c;
+let b=56;
+console.log(a+b);
+console.log(a++);
+console.log(a--);
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a%=b);
+console.log(a**=b);
+console.log(a-b)
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a&&b);
+console.log(a||b);
+console.log(a??b);
+if(a==b)
+    {
+    console.log("a is equal to b"); 
+}
+else
+{
+    console.log("a is not equal to b");
+}
+console.log((a!=b)?"a is equal to b":"a is not equal to b")
